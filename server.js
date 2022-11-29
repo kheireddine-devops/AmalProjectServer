08944a07f3440601052dfed3a48013463b21b1ff
kheireddine-devops2 <kheireddine.dev.ops@gmail.com>
2022-11-29 00:41:57 -0800

    config (Server) Adding Cors Config, Connection DataBase

--- server.js
+++ server.js
@@ -2,18 +2,29 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
-const formationRoutes = require('./routes/routes');
+
+const db = require("./models/index");
+db.sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+}).catch((error) => {
+    console.error('Unable to connect to the database: ', error);
+});
+
+const routes = require('./routes/routes');
 
 
 const server = express();
 
+server.use(cors());
 server.use(logger('dev'));
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
 server.use(cookieParser());
 server.use(express.static(path.join(__dirname, 'public')));
 
-server.use('', formationRoutes);
+
+server.use('', routes);
 
 module.exports = server;
