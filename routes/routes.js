30fd011b3db72c13c840f889ccef2f2a44cc8fd0
kheireddine-devops2 <kheireddine.dev.ops@gmail.com>
2022-12-01 03:04:48 -0800

    feat (routes) Adding Users Routes

--- routes/routes.js
+++ routes/routes.js
@@ -13,13 +13,27 @@ router.post('/formations/add',FormationController.addFormation );
 router.put('/formations/edit/:id', FormationController.editFormation);
 router.delete('/formations/delete/:id', FormationController.deleteFormation);
 
-router.all('*', AuthMiddleware.IsAuth);
 
 router.get('/accounts', AccountController.getAllAccounts);
+router.get('/accounts/:id', AccountController.getAccountByID);
+router.post('/accounts/exist-by-username', AccountController.existsAccountByUsername);
+router.post('/accounts/exist-by-email', AccountController.existsAccountByEmail);
+router.post('/accounts/exist-by-phone', AccountController.existsAccountByPhone);
+router.post('/doctors/exist-by-cin', UserController.existsDoctorByCIN);
+router.post('/doctors/exist-by-matricule', UserController.existsDoctorByMatricule);
+router.post('/organizations/exist-by-matricule', UserController.existsOrganizationByMatricule);
+router.post('/beneficiers/exist-by-catre', UserController.existsBeneficierByCarteHandicapNumber);
 router.get('/users', UserController.getAllUsers);
+router.post('/doctors', UserController.addDoctor);
 router.get('/doctors', UserController.getAllDoctors);
 router.get('/organizations', UserController.getAllOrganizations);
 router.get('/benevoles', UserController.getAllBenevoles);
 router.get('/beneficiers', UserController.getAllBeneficiers);
+
+
+
+router.all('*', AuthMiddleware.IsAuth);
+
+
 
 module.exports = router;
