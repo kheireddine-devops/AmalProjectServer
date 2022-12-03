f76a59ce36f831202a66a92786292c4244ef2ede
asma <asmaa.chaouachi@gmail.com>
2022-12-03 12:29:07 +0100

    ajouter les routes du module gestion formation

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+
+
 const FormationController = require("../controllers/formations.controller");
+const playlistsController = require("../controllers/playlists.controller");
+const VideoController = require("../controllers/videos.controller");
 const UserController = require("../controllers/users.controller");
 const AccountController = require("../controllers/accounts.controller");
 const EmploiController = require("../controllers/emplois.controller");
@@ -10,10 +14,28 @@ const AuthMiddleware = require("../middlewares/AuthMiddleware");
 
 router.post('/auth', AccountController.login);
 
-router.get('/formations', FormationController.getAllFormations);
-router.post('/formations/add',FormationController.addFormation );
+//route  annoce formations (asma)
+router.get('/formations/', FormationController.getAllFormations);
+router.get('/formations/get/:id', FormationController.getFormationById);
+router.post('/formations/add/',FormationController.addFormation );
 router.put('/formations/edit/:id', FormationController.editFormation);
 router.delete('/formations/delete/:id', FormationController.deleteFormation);
+
+//route des  playliste  (asma)
+
+router.get('/playlists/', playlistsController.getAllPlaylists);//include id-compte
+router.get('/playlist/get/:id', playlistsController.getPlaylist);//include compte
+router.post('/playlists/add/',playlistsController.addPlaylist );//include compte
+router.put('/playlists/edit/:id', playlistsController.editPlaylist);//include compte
+router.delete('/playlists/delete/:id', playlistsController.deletePlaylist);//cascade dans le model
+
+
+//route des video d'une playliste  (asma)
+
+
+router.post('/playlists/video/add',VideoController.uploadVideo );//include playliste
+router.delete('/playlists/video/delete/:id', VideoController.deleteVideo);//cascade
+
 
 
 //route sabrine
@@ -47,7 +69,7 @@ router.get('/benevoles', UserController.getAllBenevoles);
 router.get('/beneficiers', UserController.getAllBeneficiers);
 
 
-router.all('*', AuthMiddleware.IsAuth);
+// router.all('*', AuthMiddleware.IsAuth);
 
 
 
