81eaa9d3d0d218572944fd7a3c695daf11dd7d77
hasnisabrine <sabrinehasni3@gmail.com>
2022-12-07 14:23:36 +0100

    join candidature

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const FormationController = require("../controllers/formations.controller");
+const ProduitController = require("../controllers/Produits.controller");
+const AvisController = require("../controllers/Avis.controller");
 const UserController = require("../controllers/users.controller");
 const AccountController = require("../controllers/accounts.controller");
 const EmploiController = require("../controllers/emplois.controller");
@@ -16,6 +18,22 @@ router.get('/formations', FormationController.getAllFormations);
 router.post('/formations/add',FormationController.addFormation );
 router.put('/formations/edit/:id', FormationController.editFormation);
 router.delete('/formations/delete/:id', FormationController.deleteFormation);
+
+/******************************Produit ***********************************/
+
+router.get('/produits', ProduitController.getAllProduits);
+router.get('/produit/:id',ProduitController.getOneProduit);
+router.post('/createproduit',ProduitController.addProduit );
+router.put('/produit/update/:id', ProduitController.editProduit);
+router.delete('/produit/delete/:id', ProduitController.deleteProduit);
+
+
+/******************************Avis***********************************/
+
+router.get('/avis', AvisController.getAllAvis);
+router.post('/createavis',AvisController.addAvis );
+router.put('/avis/update/:id', AvisController.editAvis);
+router.delete('/avis/delete/:id', AvisController.deleteAvis);
 
 
 //route sabrine
@@ -61,8 +79,5 @@ router.get('/benevoles', UserController.getAllBenevoles);
 router.get('/beneficiers', UserController.getAllBeneficiers);
 
 
-router.all('', AuthMiddleware.IsAuth);
-
-
-
+router.all('*', AuthMiddleware.IsAuth);
 module.exports = router;
